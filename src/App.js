c3b21966ae3644c0b55084a928078d98218e919c
unknown <puthumannilrumaiz@gmail.com>
2022-05-04 23:11:55 +0530

    first commit 3

--- src/App.js
+++ src/App.js
@@ -15,7 +15,36 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Rumaise reactjs
+          Learn Rumaise reactjs he Batman is a 2022 American superhero film
+          based on the DC Comics character Batman. Produced by Warner Bros.
+          Pictures, DC Films, 6th & Idaho, and Dylan Clark Productions, and
+          distributed by Warner Bros. Pictures, it is a reboot of the Batman
+          film franchise. The film was directed by Matt Reeves, who wrote the
+          screenplay with Peter Craig. It stars Robert Pattinson as Bruce Wayne
+          / Batman alongside Zoë Kravitz, Paul Dano, Jeffrey Wright, John
+          Turturro, Peter Sarsgaard, Andy Serkis, and Colin Farrell. The film
+          sees Batman, who has been fighting crime in Gotham City for two years,
+          uncover corruption while pursuing the Riddler (Dano), a serial killer
+          who targets Gotham's elite. Development began after Ben Affleck was
+          cast as Batman in the DC Extended Universe (DCEU) in 2013. Affleck
+          signed on to direct, produce, co-write, and star in The Batman, but
+          had reservations about the project and dropped out. Reeves took over
+          and reworked the story, removing the DCEU connections. He sought to
+          explore Batman's detective side more than previous films, drawing
+          inspiration from the films of Alfred Hitchcock and the New Hollywood
+          era, and comics such as Year One (1987), The Long Halloween (1996–97),
+          and Ego (2000). Pattinson was cast in May 2019, with further casting
+          in late 2019. Principal photography took place in the UK and Chicago
+          between January 2020 and March 2021. The Batman premiered at Lincoln
+          Center, New York, on March 1, 2022, and was theatrically released
+          worldwide on March 4. It was delayed twice from an initial June 2021
+          release date due to the COVID-19 pandemic. The film has grossed over
+          $764 million against a $185–200 million budget, making it the
+          highest-grossing film of 2022, and received praise for the
+          performances, cinematography, Reeves' direction, action sequences, and
+          story, though some criticized its runtime. It is intended to launch a
+          Batman shared universe, with two sequels planned and two spin-off
+          television series in development for HBO Max.
         </a>
       </header>
     </div>
